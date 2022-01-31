62211011a60a4afbfc5a6ba8962b66320e3e7a41
lh <buffpal2016@gmail.com>
2022-01-31 09:45:07 -0500

    优化算法

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,30 @@ use wasm_bindgen::{convert::IntoWasmAbi, prelude::*};
 use wasm_bindgen::{Clamped, JsCast};
 use web_sys::{self, console, ImageData};
 
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn time(s: &str);
+
+    #[wasm_bindgen(js_namespace = console, js_name = timeEnd)]
+    fn time_end(s: &str);
+
+    // The `console.log` is quite polymorphic, so we can bind it with multiple
+    // signatures. Note that we need to use `js_name` to ensure we always call
+    // `log` in JS.
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
+    // Multiple arguments too!
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
+
 #[wasm_bindgen(start)]
 pub fn init_start() {
     console_error_panic_hook::set_once();
@@ -20,12 +44,6 @@ pub struct VideoFilter {
     inputCanvas: web_sys::HtmlCanvasElement,
     outputCanvas: web_sys::HtmlCanvasElement,
     qr: DynamicImage,
-}
-
-fn gen_qrcode(url: &str) -> DynamicImage {
-    let code = QrCode::new(url.as_bytes()).unwrap();
-    let imgBuff = code.render::<image::Luma<u8>>().build();
-    DynamicImage::ImageLuma8(imgBuff)
 }
 
 #[wasm_bindgen]
@@ -44,7 +62,7 @@ impl VideoFilter {
         }
     }
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen]
     pub fn render(&mut self) -> Result<(), JsValue> {
         let width = self.inputCanvas.width() as u32;
         let height = self.inputCanvas.height() as u32;
@@ -63,7 +81,7 @@ impl VideoFilter {
             input_canvas_context.get_image_data(0.0, 0.0, width as f64, height as f64)?;
 
         output_canvas_context.put_image_data(
-            &self.imageFilter(&input_image_data, width, height),
+            &self.imageFilter(input_image_data, width, height),
             0.0,
             0.0,
         )?;
@@ -71,42 +89,55 @@ impl VideoFilter {
         Ok(())
     }
 
-    fn imageFilter(&self, image: &ImageData, width: u32, height: u32) -> ImageData {
-        let mut imgData = image.data().to_vec();
-        let mut imgDyn = image::DynamicImage::new_rgba8(width, height);
-        let length = width * height;
-        let yLen = length / width;
-        let xLen = length / height;
+    fn imageFilter(&self, image: ImageData, width: u32, height: u32) -> ImageData {
+        time("ab");
+        let mut imgData = photon_rs::PhotonImage::from(image);
 
-        for y in 0..yLen {
-            for x in 0..xLen {
-                let slicePosition = (y as usize * width as usize + x as usize) * 4;
-                let red = imgData.get(slicePosition + 0).unwrap();
-                let green = imgData.get(slicePosition + 1).unwrap();
-                let blue = imgData.get(slicePosition + 2).unwrap();
-                let alpha = imgData.get(slicePosition + 3).unwrap();
+        photon_rs::monochrome::grayscale(&mut imgData);
 
-                let pixel = image::Rgba([*red, *green, *blue, *alpha]);
-                imgDyn.put_pixel(x, y, pixel);
-            }
-        }
-
-        // let imgDyn = imgDyn.blur(82.2);
-        // let imgDyn = imgDyn.brighten(42);
-
-        let x = imgDyn.width() - self.qr.width();
-        let y = imgDyn.height() - self.qr.height();
-
-        image::imageops::overlay(&mut imgDyn, &self.qr, x, y);
-
-        let output: &[u8] = &*imgDyn.to_rgba8().to_vec();
-
-        let output =
-            web_sys::ImageData::new_with_u8_clamped_array_and_sh(Clamped(&output), width, height)
-                .unwrap();
-
+        let output = imgData.get_image_data();
+        time_end("ab");
         return output;
     }
+
+    // fn imageFilter(&self, image: &ImageData, width: u32, height: u32) -> ImageData {
+    //     time("ab");
+    //     let imgData = image.data();
+    //     let mut imgDyn = image::DynamicImage::new_rgba8(width, height);
+    //     let length = width * height;
+    //     let yLen = length / width;
+    //     let xLen = length / height;
+    //     for y in 0..yLen {
+    //         for x in 0..xLen {
+    //             let slicePosition = (y as usize * width as usize + x as usize) * 4;
+    //             let red = imgData.get(slicePosition + 0).unwrap();
+    //             let green = imgData.get(slicePosition + 1).unwrap();
+    //             let blue = imgData.get(slicePosition + 2).unwrap();
+    //             let alpha = imgData.get(slicePosition + 3).unwrap();
+
+    //             let pixel = image::Rgba([*red, *green, *blue, *alpha]);
+    //             imgDyn.put_pixel(x, y, pixel);
+    //         }
+    //     }
+
+    //     // let imgDyn = imgDyn.blur(2.1);
+    //     let mut imgDyn = imgDyn.grayscale();
+
+    //     // let x = imgDyn.width() - self.qr.width();
+    //     // let y = imgDyn.height() - self.qr.height();
+
+    //     // image::imageops::overlay(&mut imgDyn, &self.qr, x, y);
+
+    //     let output: &[u8] = &*imgDyn.to_rgba8().to_vec();
+
+    //     let output =
+    //         web_sys::ImageData::new_with_u8_clamped_array_and_sh(Clamped(&output), width, height)
+    //             .unwrap();
+
+    //     time_end("ab");
+
+    //     return output;
+    // }
 }
 
 #[wasm_bindgen]
@@ -133,4 +164,10 @@ pub fn get_grayscale_image(_array: &[u8]) -> Vec<u8> {
     // return Vec::from(a);
 
     return (*_array).to_vec();
+}
+
+fn gen_qrcode(url: &str) -> DynamicImage {
+    let code = QrCode::new(url.as_bytes()).unwrap();
+    let imgBuff = code.render::<image::Luma<u8>>().build();
+    DynamicImage::ImageLuma8(imgBuff)
 }
